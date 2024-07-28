ae61fc228bb73aa8ed7c795313cc8218862bb5bc
rifqee23 <rifqifebrianto746@gmail.com>
2024-07-28 18:52:01 +0700

    fix layout style not publish page

--- src/Pages/NoPublishPage.jsx
+++ src/Pages/NoPublishPage.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const NoPublishPage = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
-      <h1 className="text-3xl font-bold mb-4">
+      <h1 className="text-3xl font-bold mb-4 text-center">
         This Project is Not Published Yet
       </h1>
       <p className="text-lg text-gray-700 mb-8">
