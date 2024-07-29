8efdd0cc1344f201f8323d9d65f218893cae1496
rifqee23 <rifqifebrianto746@gmail.com>
2024-07-29 16:34:51 +0700

    update logo

--- src/components/Layouts/Navbar.jsx
+++ src/components/Layouts/Navbar.jsx
@@ -15,16 +15,16 @@ const Navbar = (props) => {
     <>
       <div className="mb-10 lg:mb-0">
         <nav
-          className={`flex w-full justify-between items-center fixed top-0 right-0 left-0 lg:px-20 px-8 py-2 z-50  ${
+          className={`flex w-full justify-between items-center fixed top-0 right-0 left-0 py-3 lg:px-20 px-8  z-50  ${
             nav
               ? `flex-col bg-blue-300 animate-slideDown`
               : "bg-white animate-slideUp"
           }`}
         >
-          <a href="#" className="text-3xl font-bold text-slate-700">
-            Rifqi
+          <a href="#">
+            <img src="/logo.png" alt="logo" width={60} height={60} />
           </a>
-          <div className={`absolute top-0 right-5 md:hidden `}>
+          <div className={`absolute top-3 right-5 md:hidden `}>
             <Button onClick={handleNav}>
               {nav ? <IoCloseSharp size={40} /> : <RxHamburgerMenu size={40} />}
             </Button>
@@ -35,7 +35,9 @@ const Navbar = (props) => {
             }`}
           >
             <ul
-              className={`flex gap-4 ${nav && `flex-col justify-center mt-5`}`}
+              className={`flex items-center gap-x-4 mt-2 ${
+                nav && `flex-col justify-center mt-5`
+              }`}
             >
               <li className="font-semibold text-lg text-center text-slate-700 hover:text-slate-400">
                 <a href="#">Home</a>
